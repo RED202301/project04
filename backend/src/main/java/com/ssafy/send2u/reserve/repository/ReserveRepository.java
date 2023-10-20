02fd526ce9553f8f560cf083656ffbbaaa7e11dc
홍진환 <ghdtka1218@gmail.com>
2023-10-20 17:58:19 +0900

    ✨[BE] Feat: create reservepackage

--- backend/src/main/java/com/ssafy/send2u/reserve/repository/ReserveRepository.java
+++ backend/src/main/java/com/ssafy/send2u/reserve/repository/ReserveRepository.java
@@ -4,5 +4,5 @@ import com.ssafy.send2u.reserve.entity.Reserve;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReserveRepository extends JpaRepository<Reserve, Long> {
-    void deleteReserve(Long id);
+
 }
