ad06344e73a1f5b4047993888b3432e04b755d62
정재범 <zxsefv@gmail.com>
2023-11-07 08:53:08 +0900

    :sparkles: [FE]Feat: 튜토리얼 수정

--- frontend/src/routes/View/components/Tutorial.tsx
+++ frontend/src/routes/View/components/Tutorial.tsx
@@ -8,12 +8,6 @@ export const tourOptions = {
   
   export const tourSteps = [
     {
-      title: "공유 버튼",
-      element: "#share",
-      intro:
-        "내 페이지를 <b>공유</b> 해보세요.",
-    },
-    {
       title: "목록 버튼",
       element: "#floating-button",
       intro:
@@ -29,27 +23,33 @@ export const tourOptions = {
       position: "top"
     },
     {
+        title: "목록-비밀편지 모아보기",
+        element: "#ID1",
+        intro: "클릭 시 비밀편지를 <br> 모아볼 수 있습니다.",
+        position: "top-left-aligned"
+      },
+    {
+        title: "목록-홈",
+        element: "#ID2",
+        intro: "클릭 시 페이지로 <br> 이동할 수 있습니다.",
+        position: "top"
+    },
+    {
       title: "목록-메세지 모아보기",
-      element: "#ID1",
+      element: "#ID3",
       intro: "클릭 시 메세지를 <br> 모아볼 수 있습니다.",
       position: "top"
-    },
-    {
-      title: "목록-홈",
-      element: "#ID2",
-      intro: "클릭 시 페이지로 <br> 이동할 수 있습니다.",
-      position: "top"
-    },
-    {
-      title: "목록-비밀편지 모아보기",
-      element: "#ID3",
-      intro: "클릭 시 비밀편지를 <br> 모아볼 수 있습니다.",
-      position: "top-left-aligned"
     },
     {
       title: "메세지",
       element: "#message",
       intro: "클릭 시 메세지를 <br> 확대해서 볼 수 있습니다. <br> 꾹 누르면 메세지를 <br> 자유롭게 배치할 수 있습니다.",
     },
+    {
+        title: "공유 버튼",
+        element: "#share",
+        intro:
+          "내 페이지를 <b>공유</b> 해보세요.",
+      },
   ];
   
--- frontend/src/routes/View/components/TutorialButton.tsx
+++ frontend/src/routes/View/components/TutorialButton.tsx
@@ -3,6 +3,8 @@ import "intro.js/introjs.css";
 import { Steps } from "intro.js-react";
 import { tourOptions, tourSteps } from "./Tutorial";
 import {BsQuestionCircle} from "react-icons/bs"
+import { useRecoilState } from "recoil";
+import isActiveFloatingState from "../../../recoil/isActiveFloatingState";
 
 const TutorialButton: React.FC = () => {
     const [stepsEnabled, setStepsEnabled] = useState(false);
@@ -16,8 +18,10 @@ const TutorialButton: React.FC = () => {
       };
     const handleHelp = () => {
         setStepsEnabled((prev) => !prev);
+        setIsActiveFloating(true)
       };
     const ICON = BsQuestionCircle
+    const [isActiveFloating, setIsActiveFloating] = useRecoilState(isActiveFloatingState)
 
     return(
         <div>
