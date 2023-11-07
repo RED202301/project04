5d9d26bd13f62825c59787279b7892b7e97442d2
김동현 <kdhdh001@naver.com>
2023-11-07 10:41:52 +0900

    :bug: fix: article CRUD 수정완료

--- backend/src/main/java/com/ssafy/send2u/article/entity/Article.java
+++ backend/src/main/java/com/ssafy/send2u/article/entity/Article.java
@@ -2,35 +2,40 @@ package com.ssafy.send2u.article.entity;
 
 
 import com.ssafy.send2u.user.entity.user.User;
-import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Setter
 @Getter
-
+@EntityListeners(AuditingEntityListener.class)
 @Entity
+@Table(name = "article")
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long articleId;
+
     private String longText;
-    @ApiModelProperty(hidden = true)
-    private String senderId;
+
     private String articleTitle;
-    private String articleFileUrl;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "articlesender_id")
-    private User articleSender;
+    @JoinColumn(name = "articlewriter_id")
+    private User articleWriter;
+
     @CreatedDate
     private LocalDateTime date;
-    public Article() {
-        // Default constructor
-    }
 
-    // Getters and setters
 }
--- backend/src/main/java/com/ssafy/send2u/article/repository/ArticleRepository.java
+++ backend/src/main/java/com/ssafy/send2u/article/repository/ArticleRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.send2u.article.repository;
 
 import com.ssafy.send2u.article.entity.Article;
+import com.ssafy.send2u.user.entity.user.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findByArticleWriter(User writer);
 
 }
