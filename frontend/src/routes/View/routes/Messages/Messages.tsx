25c6e08fec26ace0c41576eb7e660100312f596a
정재범 <zxsefv@gmail.com>
2023-11-08 14:41:20 +0900

    :sparkles: [FE]Feat: 모아보기 헤더 추가

--- frontend/src/routes/View/routes/Messages/Messages.tsx
+++ frontend/src/routes/View/routes/Messages/Messages.tsx
@@ -24,11 +24,11 @@ const Messages = () => {
   // ]
 
   return <Fragment>
+    <p css={tw`absolute left-[center] top-0 text-white text-2xl bg-[rgba(0,0,0,0.5)] w-[100%] text-center z-20`}>메세지 모아보기</p>
     {messages.map(message => {
       if(message.type === 1) return <div {...{css:tw`m-1 z-10`, key:message.id}}><Stickynote {...{...message, sizeRatio:.3 }} /></div>
       if(message.type === 2) return <div {...{css:tw`m-1 z-10`, key:message.id}}><Polaroid {...{...message, sizeRatio:.3 }} /></div>
     })}
-    
   </Fragment>
 }
 
